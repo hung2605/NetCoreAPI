048c09e5f32c5a47236bea23c905005050976bc8
Your Name <you@example.com>
2025-08-28 13:02:56 +0700

    .

---
+++ DEMO_MVC/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace DEMO_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string FullName, string Address)
+        {
+            string strOutput = "Xin chào " + FullName + " đến từ " + Address;
+            ViewBag.Message = strOutput;
+            return View();
+        }
+    }
+}
